3638f5bc20f9b38b4e88ba2022216b644f5bc32d
Mykhailo Kan <mykhailo.kan@giss.pl>
2023-10-11 17:03:54 +0200

    merge done message

--- fan_in.go
+++ fan_in.go
@@ -1,6 +1,9 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 // merges multiple chans into one
 // out chan after closing rapidly returns value, ok
@@ -11,6 +14,8 @@ func Merge[T any](done <-chan struct{}, cs ...<-chan T) <-chan T {
 
 	output := func(c <-chan T) {
 		defer wg.Done()
+		defer func() { fmt.Println("Merge routine done") }()
+
 		for v := range c { // finishes when c (chan) is closed
 			select {
 			case <-done:
